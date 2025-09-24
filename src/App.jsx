cfafedb79cc196d53a5fd8ca6e4d7e12eccee631
NUCodeX <nucodex69@gmail.com>
2025-09-24 22:49:22 +0800

    updated teact code

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     
       <Routes>
-        <Route path="/Alexandra" element={<Landing />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/choices" element={<Options />} />
 
         <Route path="/journey" element={<Journey />} />
